ee8b11b6c55227ffef1d3f652fbad50788d7c335
satish <satishkonduru0225@gmail.com>
2024-05-10 09:24:55 +0530

    New Project with initial setup

---
+++ SDLC/src/app/modules/angular-material/angular-material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+
+
+const materialComponents = [
+
+]
+@NgModule({
+  imports: [materialComponents],
+  exports: [materialComponents]
+})
+export class AngularMaterialModule { }
